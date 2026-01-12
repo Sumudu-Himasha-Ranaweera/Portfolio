9d66981de3714ec6c176ac16530f07e83af0797b
Himasha Ranaweera <sumuranaweera@gmail.com>
2026-01-12 09:02:46 +0530

    fix: Update CV download link to include proper formatting for improved accessibility

--- src/components/MailchimpForm.js
+++ src/components/MailchimpForm.js
@@ -24,7 +24,7 @@ export const MailchimpForm = () => {
                 </svg>
                 <span>+94 77 235 2512</span>
               </a>
-              <a href="/cv/Himasha_Ranaweera_CV.pdf" download className="download-cv-btn">
+              <a href="/cv/HIMASHA%20RANAWEERA%20-%20CV.pdf" download className="download-cv-btn">
                 <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
                   <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"/>
                   <polyline points="7 10 12 15 17 10"/>
